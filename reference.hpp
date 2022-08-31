4167ee96ba4a7a37db69eec3ebd6867fd8cc1012
Koter Romanov <guole535411178@gmail.com>
2022-08-31 23:55:47 +0800

    添加数组的引用、指针的引用实践和练习

--- reference.hpp
+++ reference.hpp
@@ -24,11 +24,35 @@ void func_reference()
 	const int& rf_dat_const = dat; // 引用之后引用是常量，不能通过引用改变变量值
 
 
-	// 引用字面常量
-
+	// 常量引用，引用字面值
+	const int& rf_const = 100;
+	cout << "rf_const :" << rf_const << endl;
 
 	// 引用数组
+	int arry_dat[10] = { 1, 2, 3, 4, 5 };
+	int(&rf_arry_dat)[10] = arry_dat; // rf_arry_dat 首先和 & 结合，所以 rf_arry_dat 是引用，引用的对象是数组
+	cout << arry_dat << endl;
+	cout << rf_arry_dat << endl;
+	for (int i = 0; i < 10; i++) {
+		cout << rf_arry_dat[i] << endl;
+	}
+
+	// 扩展: 引用数组（数组里面保存的是引用）
+	// int& arry_dat_[2]; (引用的数组是非法的)
 
 
 	// 指针引用
+	int* point_dat = &dat;
+	int*& rf_point_dat = point_dat;  // 指针的引用，指针 point_dat 的别名
+	cout << "point_dat address: " << point_dat << endl;
+	cout << "rf_point_dat addres: " << rf_point_dat << endl;
+
+	// 练习：指针的引用的用法
+	cout << "dat data from reference: " << *rf_point_dat << endl;
+
+	int new_dat = 200;
+	rf_point_dat = &new_dat; // 通过引用让 point_dat 指针指向新的变量
+	cout << "point_dat address: " << point_dat << endl;
+	cout << "rf_point_dat addres: " << rf_point_dat << endl;
+	cout << "dat data from reference: " << *rf_point_dat << endl;
 }
