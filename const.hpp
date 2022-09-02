f0c9127565c7a0b64eaeda39a58c659a34764411
Koter Romanov <guole535411178@gmail.com>
2022-09-02 23:50:30 +0800

    添加常指针、指针常量代码

--- const.hpp
+++ const.hpp
@@ -6,10 +6,34 @@
  */
 void func_const()
 {
+
+	// 效果一样
 	const int Remote = 100;
 	int const Local = 100;
+	int tmpData = 300;
+	int tmpLocal = 300;
 
 	// Remote = 200; （错误：不允许修改常量）
 	
 	// const & int p3 （错误：const 和被修饰类型之间不能有其他标识符。 ps：& 符不是类型）
+
+	
+	// 常指针：指针指向一个常量
+	// const 修饰 int ，表示指针指向的是一个常量，不可以通过指针修改变量的值
+	int const *pConstRemote = &tmpData;
+	//*pConstRemote = 200; // 报错，不允许通过指针修改变量的值
+
+	// 没有 const 修饰的指针变量，可以通过指针修改变量的值
+	int* pRemote = &tmpData;
+	*pRemote = 200;
+
+
+	// 指针常量：指针是一个常量
+	// const 修饰 int*，表示指针是一个常量，不允许修改指针的指向
+	int* const pConstLocal = &tmpData;
+	//pConstLocal = &Local; // 报错，不允许修改指针的指向
+
+	// 没有 const 修改的指针，可以改变指针的指向
+	int* pLocal = &tmpData;
+	pLocal = &tmpLocal;
 }
