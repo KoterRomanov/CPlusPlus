817f3cb7e630bea089146bdd79b954d997b57c23
Koter Romanov <guole535411178@gmail.com>
2022-09-15 23:13:21 +0800

    添加基本类型练习

--- datatype.hpp
+++ datatype.hpp
@@ -2,10 +2,21 @@
 /**
  * @brief   对 C++ 数据类型的练习
  * @author  koterromanov
- * @date    2022-09-14
+ * @date    2022-09-15
  */
+#include "define.h"
 
 void function_datatype()
 {
+	bool ret = false;
 
+	if (ret) {
+		cout << "Remote" << endl;
+	}
+	else {
+	
+		cout << "Local" << endl;
+	}
+
+	std::cout.imbue(std::locale("chs"));
 }
