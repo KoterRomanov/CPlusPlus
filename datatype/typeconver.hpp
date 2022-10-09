2fa9bbf68ca709fd3c4fd598d4b4d53bff13aabb
Koter Romanov <guole535411178@gmail.com>
2022-10-09 23:07:46 +0800

    添加 static_cast 关键字练习

--- datatype/typeconver.hpp
+++ datatype/typeconver.hpp
@@ -12,7 +12,11 @@ void function_type_convert()
 	const unsigned char cpbuffer[] = {"hello"};
 	unsigned char* pbuffer = const_cast<unsigned char*>(cpbuffer);
 
-	/* static_cast */
+	/* static_cast 用于强制类型转换*/
+	int type = 100;
+	unsigned char ch_type = static_cast<unsigned char>(type);
+
+	/* static_cast 用于存在继承关系的类类型之间的转换 */
 
 	/* dynamic_cast */
 
