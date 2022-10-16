ad0308f6087dc0b6ae84293fcb4066df3e978375
Koter Romanov <guole535411178@gmail.com>
2022-10-16 18:11:47 +0800

    添加函数定义、声明练习

--- datatype/declare.hpp
+++ datatype/declare.hpp
@@ -4,15 +4,23 @@
  * @author  koterromanov
  * @date    2022-10-15
  */
+#include "define.h"
 
 int g_data; /* 变量定义，分配了内存，默认初始化为 0 */
 
-extern int g_remote; /* 变量声明，没有分配内存，是对已有变量的引用 */
+extern int g_remote; /* 变量声明，没有分配内存，是对已有变量的引用，允许出现不同的源文件内 */
 
 extern int g_local = 0; /* 变量定义，分配了内存，显式初始化为 0 */
 
+/* 函数声明 */
+void test_function();
+
 void function_declare()
 {
+	printf("fucntion declare\n");
+}
 
-
+void test_funcion()
+{
+	printf("This is a test function!\n");
 }
