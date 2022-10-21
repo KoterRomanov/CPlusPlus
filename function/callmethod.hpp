a86eafd561a3247a4ec8e5941602738c96762b32
Koter Romanov <guole535411178@gmail.com>
2022-10-21 23:50:13 +0800

    添加stdcall、cdecl 两种调用约定练习

--- function/callmethod.hpp
+++ function/callmethod.hpp
@@ -1,1 +1,19 @@
 #pragma once
+
+#include <iostream>
+
+/* c/c++ 标准函数调用约定 */
+void __stdcall test_stdcall(int a, int b)
+{
+
+}
+
+/* c、c++ 默认函数调用约定 */
+void __cdecl test_cdecl(int a, int b)
+{
+
+}
+
+void function_call()
+{
+}
