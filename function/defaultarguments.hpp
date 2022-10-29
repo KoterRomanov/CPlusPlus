abedff74765299cbe95d31114945f8ba51c291e4
Koter Romanov <guole535411178@gmail.com>
2022-10-29 23:46:47 +0800

    完善函数默认值练习

--- function/defaultarguments.hpp
+++ function/defaultarguments.hpp
@@ -1,6 +1,12 @@
 #pragma once
 
 #include "define.h"
+/*
+ * 1. 函数声明时指定参数的默认值, 定义时添加注释说明默认值是什么 
+ * 2. 任何一个不带默认值的参数都要出现在所有带默认值的参数的左边（避免函数调用时参数匹配的二义性）
+ * 3. 函数参数的默认值需要注意不能造成函数重载的二义性（例如 void show(int x = 10, double y = 9.9);  void show(int x);  show(1);说调用哪个函数无法确定的二义性）
+ *
+ */
 
 void function_default_arguments(int value = 100)
 {
