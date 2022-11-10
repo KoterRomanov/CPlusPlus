8626138518118e7aef29838e5a5583c1f2748eb1
Koter Romanov <guole535411178@gmail.com>
2022-11-10 23:34:43 +0800

    补充函数指针练习

--- function/functionpointer.hpp
+++ function/functionpointer.hpp
@@ -8,12 +8,25 @@
 
 #include "define.h"
 
+/* 函数指针变量声明 */
 int (*pMyPrintf)(const char* fmt, ...);
 
+/* 声明函数指针类型别名 */
+typedef int(*printfType)(const char*, ...);
 
 void function_pointer()
 {
+	/* 方式一：函数指针变量赋值 */
 	pMyPrintf = printf;
+
+	/* 方式二：函数指针定义 + 赋值 */
+	int (*pMyPrintf_)(const char*, ...) = printf;
+
+	/* 方式三：使用别名定义函数指针变量 */
+	printfType pFunc = printf;
+
+	/* 方式四：取地址方式赋值函数地址（与方式三等价） */
+	pFunc = &printf;
 
 	pMyPrintf("The is test function pointer: %d\n", 100);
 }
